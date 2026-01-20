d3f69aab126ae194a05b85fb7105c6d4820d9af2
Rohank1442 <rohankumar1442@gmail.com>
2026-01-20 18:12:40 +0530

    bug-fix

--- server/app/main.py
+++ server/app/main.py
@@ -6,21 +6,17 @@ app = FastAPI(title="AI Essay Writer")
 
 origins = [
     "http://localhost:8080",
-    "http://127.0.0.1:8080",
-    "http://192.168.1.14:8080",
-    "paperflow-7tn3pw27z-rohans-projects-c605bf65.vercel.app"
-    "https://paperflow-ashen.vercel.app",
+    "https://paperflow-ai.vercel.app",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,            # Allows your frontend
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],               # Allows POST, GET, OPTIONS, etc.
-    allow_headers=["*"],               # Allows Content-Type, Authorization, etc.
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-# Include Routers
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 app.include_router(documents.router, prefix="/documents", tags=["Documents"])
 app.include_router(essays.router, prefix="/files", tags=["Files"])
